ca47af689dcf6a22535385c0e46f62e8521f1668
siddharth tutorial <ggvanecha@gmail.com>
2025-05-03 13:04:18 +0530

    data file

---
+++ src/data/questions.js
@@ -0,0 +1,75 @@
+const questions = [
+    {
+      question: "Which language is used to style web pages?",
+      options: ["HTML", "CSS", "JavaScript", "Python"],
+      answer: "CSS",
+    },
+    {
+      question: "Which HTML tag is used for inserting a line break?",
+      options: ["<break>", "<br>", "<lb>", "<line>"],
+      answer: "<br>",
+    },
+    {
+      question: "What is the correct syntax to reference an external JavaScript file?",
+      options: [
+        "<script href='script.js'>",
+        "<javascript src='script.js'>",
+        "<script src='script.js'>",
+        "<js src='script.js'>",
+      ],
+      answer: "<script src='script.js'>",
+    },
+    {
+      question: "Which is a frontend JavaScript framework?",
+      options: ["Node.js", "Express", "React", "MongoDB"],
+      answer: "React",
+    },
+    {
+      question: "What does 'npx' stand for and what is its primary use?",
+      options: [
+        "Node Package Executor - to run Node.js applications",
+        "Node Package Express - to install global packages",
+        "Node Package Executor - to run packages without installing globally",
+        "New Package Extension - to create React apps"
+      ],
+      answer: "Node Package Executor - to run packages without installing globally"
+    },
+    {
+      question: "Which tool is used to manage source code versions?",
+      options: ["Docker", "Git", "Webpack", "NPM"],
+      answer: "Git",
+    },
+    {
+      question: "What does GitHub primarily provide?",
+      options: [
+        "Cloud hosting for apps",
+        "Version control system",
+        "Remote Git repositories",
+        "Code compiler",
+      ],
+      answer: "Remote Git repositories",
+    },
+    {
+      question: "Which command is used to upload local code to GitHub?",
+      options: ["git push", "git upload", "git merge", "git send"],
+      answer: "git push",
+    },
+    {
+      question: "Which file is used to ignore files from being committed to Git?",
+      options: [".gitignore", "ignore.txt", ".gitconfig", "commitignore"],
+      answer: ".gitignore",
+    },
+    {
+      question: "What does 'npm' stand for?",
+      options: [
+        "Node Project Manager",
+        "New Package Manager",
+        "Node Package Manager",
+        "Network Package Module",
+      ],
+      answer: "Node Package Manager",
+    },
+  ];
+  
+  export default questions;
+  
